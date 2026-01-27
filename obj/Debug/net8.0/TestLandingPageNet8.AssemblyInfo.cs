4f552ce6d58efef586a69eefa48345bc57053869
Mirzamuhamad <mohamadmirza27@gmail.com>
2026-01-27 22:17:27 +0700

    menambah history service Order

--- obj/Debug/net8.0/TestLandingPageNet8.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestLandingPageNet8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestLandingPageNet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd665fff6eb5aaca275a3c8ad32b41a59f93837d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddda475735e507e989488b6ae740b25549a53451")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestLandingPageNet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestLandingPageNet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
